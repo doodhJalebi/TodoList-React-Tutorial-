956dd4426bdfc41d02282350237014322188b98a
Owais <blackhawk2399@gmail.com>
2020-07-18 21:52:06 +0500

    App committed

---
+++ precache-manifest.15c9c1bf5df928b4423e10188af4dfeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16a536669c91c916707a49c3deb0c5ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe7b68e2598c81ad0652",
+    "url": "/static/css/main.36fe29b4.chunk.css"
+  },
+  {
+    "revision": "5c32f5950867c71b28bb",
+    "url": "/static/js/2.f703eb3f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f703eb3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe7b68e2598c81ad0652",
+    "url": "/static/js/main.baa4beb3.chunk.js"
+  },
+  {
+    "revision": "9f08f0e23da8310728e7",
+    "url": "/static/js/runtime-main.5b849a39.js"
+  }
+]);
